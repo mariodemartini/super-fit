0207cff496c33c7d07fb31462b72a48c72f70dd0
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-02-08 15:20:55 -0300

    :sparkle: feat: criando entidade

---
+++ super-fit/model/DadosInicias.php
@@ -0,0 +1,26 @@
+<?php
+
+    Class DadosIniciais{
+        private $idDadosIniciais;
+        private $peso;
+        private $altura;
+        private $freq_card;
+        private $pressao_arterial;
+        private $data_cadastro;
+
+        function getIdDadosIniciais()
+        {
+            return $this->IdDadosIniciais;           
+        }
+
+        function setIdDadosIniciais($idDadosIniciais)
+        {
+            $this->idDadosIniciais;
+
+        }
+
+
+
+    }
+
+?>
