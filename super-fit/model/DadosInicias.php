9b67adf7a3740f2653aac34d56cfa3eebe2e8d4b
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-02-08 16:02:10 -0300

    :recycle: refactor: concluindo entidade

--- super-fit/model/DadosInicias.php
+++ super-fit/model/DadosInicias.php
@@ -10,17 +10,58 @@
 
         function getIdDadosIniciais()
         {
-            return $this->IdDadosIniciais;           
+            return $this->idDadosIniciais;           
         }
 
         function setIdDadosIniciais($idDadosIniciais)
         {
-            $this->idDadosIniciais;
-
+            $this->idDadosIniciais = $idDadosIniciais;
         }
 
+        function getPeso()
+        {
+            return $this->peso;           
+        }
 
+        function setPeso($peso)
+        {
+            $this->peso = $peso;
+        }
 
+        function getAltura()
+        {
+            return $this->altura;           
+        }
+
+        function setAltura($altura)
+        {
+            $this->altura = $altura;
+        }
+
+        function getFreq_Card()
+        {
+            return $this->freq_card;           
+        }
+
+        function setFreq_Card($freq_card)
+        {
+            $this->freq_card = $freq_card;
+        }
+
+        function getPressao_Arterial()
+        {
+            return $this->pressao_arterial;           
+        }
+
+        function setPressao_Arterial($pressao_arterial)
+        {
+            $this->pressao_arterial = $pressao_arterial;
+        }
+        
+        function setData_Cadastro($data_cadastro)
+        {
+            $this->data_cadastro = $data_cadastro;
+        }
     }
 
 ?>
