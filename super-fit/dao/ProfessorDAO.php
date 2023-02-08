7ec1d56f23fc675856dbb2a9726343335b329b1c
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-02-08 15:20:16 -0300

    :recycle: refactor: atualizando dados

--- super-fit/dao/ProfessorDAO.php
+++ super-fit/dao/ProfessorDAO.php
@@ -101,8 +101,6 @@
                 $p_sql->bindValue(":data_nascimento", $professor->getData_Nascimento());
                 $p_sql->bindValue(":sexo", $professor->getSexo());
                 $p_sql->bindValue(":cpf", $professor->getCpf());
-                $p_sql->bindValue(":rg", $professor->getRg());
-                $p_sql->bindValue(":telefone", $professor->getTelefone());
                 $p_sql->bindValue(":celular", $professor->getCelular());
                 $p_sql->bindValue(":cep", $professor->getCep());
                 $p_sql->bindValue(":estado", $professor->getEstado());
@@ -110,7 +108,6 @@
                 $p_sql->bindValue(":endereco", $professor->getEndereco());
                 $p_sql->bindValue(":email", $professor->getEmail());
                 $p_sql->bindValue(":senha", $professor->getSenha());
-                $p_sql->bindValue(":data_cadastro", $professor->getData_Cadastro());
             
                 return $p_sql->execute();
             } catch (Exception $e) {
