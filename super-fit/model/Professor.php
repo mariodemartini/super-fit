fb7ceba6b911d4c31d85bc427aba7e12c4f736bb
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-02-15 10:42:41 -0300

    :ok_hand: sytle: melhoranco identação

--- super-fit/model/Professor.php
+++ super-fit/model/Professor.php
@@ -15,137 +15,137 @@
         private $email;
         private $senha;
 
-    function getIdProfessor()
-    { 
-        return $this->idProfessor; 
-    }
+        function getIdProfessor()
+        { 
+            return $this->idProfessor; 
+        }
 
-    function setIdProfessor($idProfessor)
-    { 
-        $this->idProfessor = $idProfessor; 
-    }
+        function setIdProfessor($idProfessor)
+        { 
+            $this->idProfessor = $idProfessor; 
+        }
 
-    function getNome()
-    { 
-        return $this->nome; 
-    }
+        function getNome()
+        { 
+            return $this->nome; 
+        }
 
-    function setNome($nome)
-    { 
-        $this->nome = $nome; 
-    }
+        function setNome($nome)
+        { 
+            $this->nome = $nome; 
+        }
 
-    function getData_Nascimento()
-    { 
-        return $this->data_nascimento; 
-    }
+        function getData_Nascimento()
+        { 
+            return $this->data_nascimento; 
+        }
 
-    function setData_Nascimento($data_nascimento)
-    { 
-        $this->data_nascimento = $data_nascimento; 
-    }
+        function setData_Nascimento($data_nascimento)
+        { 
+            $this->data_nascimento = $data_nascimento; 
+        }
 
-    function getSexo()
-    { 
-        return $this->sexo; 
-    }
+        function getSexo()
+        { 
+            return $this->sexo; 
+        }
 
-    function setSexo($sexo)
-    { 
-        $this->sexo = $sexo; 
-    }
+        function setSexo($sexo)
+        { 
+            $this->sexo = $sexo; 
+        }
 
-    function getCpf()
-    { 
-        return $this->cpf; 
-    }
+        function getCpf()
+        { 
+            return $this->cpf; 
+        }
 
-    function setCpf($cpf)
-    { 
-        $this->cpf = $cpf; 
-    }
+        function setCpf($cpf)
+        { 
+            $this->cpf = $cpf; 
+        }
 
-    function getCref()
-    { 
-        return $this->cref; 
-    }
+        function getCref()
+        { 
+            return $this->cref; 
+        }
 
-    function setCref($cref)
-    { 
-        $this->cref = $cref; 
-    }
+        function setCref($cref)
+        { 
+            $this->cref = $cref; 
+        }
 
-    function getCelular()
-    { 
-        return $this->celular; 
-    }
+        function getCelular()
+        { 
+            return $this->celular; 
+        }
 
-    function setCelular($celular)
-    { 
-        $this->celular = $celular; 
-    }
+        function setCelular($celular)
+        { 
+            $this->celular = $celular; 
+        }
 
-    function getCep()
-    { 
-        return $this->cep; 
-    }
+        function getCep()
+        { 
+            return $this->cep; 
+        }
 
-    function setCep($cep)
-    { 
-        $this->cep = $cep; 
-    }
+        function setCep($cep)
+        { 
+            $this->cep = $cep; 
+        }
 
-    function getEstado() 
-    {
-        return $this->estado;
-    }
+        function getEstado() 
+        {
+            return $this->estado;
+        }
 
-    function setEstado($estado)
-    {
-        $this->estado = $estado;
-    }
+        function setEstado($estado)
+        {
+            $this->estado = $estado;
+        }
 
-    function getCidade()
-    {
-        return $this->cidade;
-    }
-    
-    function setCidade($cidade)
-    {
-        $this->cidade = $cidade;
-    }
+        function getCidade()
+        {
+            return $this->cidade;
+        }
+        
+        function setCidade($cidade)
+        {
+            $this->cidade = $cidade;
+        }
 
-    function getEndereco()
-    {
-        return $this->endereco;
-    }
+        function getEndereco()
+        {
+            return $this->endereco;
+        }
 
-    function setEndereco($endereco)
-    {
-        $this->endereco = $endereco;
-    }
+        function setEndereco($endereco)
+        {
+            $this->endereco = $endereco;
+        }
 
-    function getEmail()
-    {
-        return $this->email;
-    }
+        function getEmail()
+        {
+            return $this->email;
+        }
 
-    function setEmail($email)
-    {
-        $this->email = $email;
-    }
+        function setEmail($email)
+        {
+            $this->email = $email;
+        }
 
-    function getSenha()
-    {
-        return $this->senha;
-    }
+        function getSenha()
+        {
+            return $this->senha;
+        }
 
-    function setSenha($senha)
-    {
-        $this->senha = $senha;
-    }
+        function setSenha($senha)
+        {
+            $this->senha = $senha;
+        }
 
-}
+    }
    
 
 ?>
