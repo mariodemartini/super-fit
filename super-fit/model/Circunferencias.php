139624123545b441ce655331905629b6f2fa1576
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-02-15 10:36:34 -0300

    :sparkle: feat: inserindo getter e setter

--- super-fit/model/Circunferencias.php
+++ super-fit/model/Circunferencias.php
@@ -2,81 +2,84 @@
     
     class Circunferencias
     {
-    private $idcircunferencias;
-    private $torax;
-    private $cintura;
-    private $abdomen;
-    private $quadril;
-    private $braco_direito;
-    private $braco_esquerdo;
-    private $antebraco_direito;
-    private $antebraco_esquerdo;
-    private $coxa_direita;
-    private $coxa_esquerda;
-    private $panturrilha_direita;
-    private $panturrilha_esquerda;
-    private $data_cadastro;
-     
-    function getidCircunferencias()
-    {
-        return $this->idcircunferencias;
-    }
-    function setidcircunferencias($idcircunferencias)
-    {
-        $this->idcircunferencias = $idcircunferencias;
-    }
-    function getTorax()
-    {
-        return $this->torax;           
-    }
-    function settorax($torax)
-    {
-        $this->torax = $torax;
-    }
-    function getCintura()
-    {
-        return $this->cintura;           
-    }
+        private $idCircunferencias;
+        private $torax;
+        private $cintura;
+        private $abdomen;
+        private $quadril;
+        private $braco_direito;
+        private $braco_esquerdo;
+        private $antebraco_direito;
+        private $antebraco_esquerdo;
+        private $coxa_direita;
+        private $coxa_esquerda;
+        private $panturrilha_direita;
+        private $panturrilha_esquerda;
+        private $data_cadastro;
+        
+        function getIdCircunferencias()
+        {
+            return $this->idCircunferencias;
+        }
+        function setIdCircunferencias($idCircunferencias)
+        {
+            $this->idCircunferencias = $idCircunferencias;
+        }
 
-    function setcintura($cintura)
-    {
-        $this->cintura = $cintura;
-    }
-    function getAbdomen()
-    {
-        return $this->abdomen;           
-    }
+        function getTorax()
+        {
+            return $this->torax;           
+        }
+        function setTorax($torax)
+        {
+            $this->torax = $torax;
+        }
 
-    function setabdomen($abdomen)
-    {
-        $this->abdomen = $abdomen;
-    }
-    function getQuadril()
-    {
-        return $this->quadril;           
-    }
+        function getCintura()
+        {
+            return $this->cintura;           
+        }
 
-    function setquadril($quadril)
-    {
-        $this->quadril = $quadril;
-    }
-    function getBraco_direito()
-    {
-        return $this->braco_direito;           
-    }
+        function setCintura($cintura)
+        {
+            $this->cintura = $cintura;
+        }
 
-    function setbraco_esquerdo($braco_esquerdo)
-    {
-        $this->braco_esquerdo = $braco_esquerdo;
-    }
-    function getPeso()
-    {
-        return $this->peso;           
-    }
+        function getAbdomen()
+        {
+            return $this->abdomen;           
+        }
+        function setAbdomen($abdomen)
+        {
+            $this->abdomen = $abdomen;
+        }
 
-    function setPeso($peso)
-    {
-        $this->peso = $peso;
-    }
+        function getQuadril()
+        {
+            return $this->quadril;           
+        }
+        function setQuadril($quadril)
+        {
+            $this->quadril = $quadril;
+        }
+
+        function getBraco_direito()
+        {
+            return $this->braco_direito;           
+        }
+        function setBraco_esquerdo($braco_esquerdo)
+        {
+            $this->braco_esquerdo = $braco_esquerdo;
+        }
+
+        function getPeso()
+        {
+            return $this->peso;           
+        }
+        function setPeso($peso)
+        {
+            $this->peso = $peso;
+        }
+
     }
 ?>
