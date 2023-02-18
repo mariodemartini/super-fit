44b4dd0eaa9accad0a6ac640ba22e65aeeee6ea4
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-02-18 15:57:07 -0300

    :sparkle: feat: criando classe controller

---
+++ super-fit/controller/SerieController.php
@@ -0,0 +1,47 @@
+<?php
+    include_once "../conexao/Conexao.php";
+    include_once "../model/Serie.php";
+    include_once "../dao/SerieDAO.php";
+
+    $serie = new Serie();
+    $seriedao = new SerieDAO();
+
+    $d = filter_input_array(INPUT_POST);
+
+    if(isset($_POST['salvar'])){
+
+        $serie->setNivel(($d['nivel']));
+        $serie->setFase(($d['fase']));
+        $serie->setSerie(($d['serie']));
+        $serie->setPercent_Carga(($d['percent_carga']));
+        
+        $seriedao->create($serie);
+
+        header("Location: ../novoexercicio.php");
+    }
+
+    else if (isset($_POST['editar'])) {
+
+        $serie->setIdSerie(($d['idSerie']));
+        $serie->setNivel(($d['nivel']));
+        $serie->setFase(($d['fase']));
+        $serie->setSerie(($d['serie']));
+        $serie->setPercent_Carga(($d['percent_carga']));
+    
+        $seriedao->update($serie);
+    
+        header("Location: ../pesquisa-serie.php");
+    }
+
+    else if (isset($_GET['del'])){
+
+        $serie->setIdSerie($_GET['del']);
+
+        $seriedao->delete($serie);
+
+        header("Location: ../pesquisa-serie.php");
+    } else {
+        header("Location: ../pesquisa-serie.php");
+    }
+    
+?>
