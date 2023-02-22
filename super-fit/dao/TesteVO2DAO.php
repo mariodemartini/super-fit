46cde95f141a1c459abf31cdd23e577a03bf368c
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-02-22 11:20:50 -0300

    :bug: fix: corrigindo variaveis

--- super-fit/dao/TesteVO2DAO.php
+++ super-fit/dao/TesteVO2DAO.php
@@ -12,14 +12,14 @@
                 $p_sql = Conexao::getConexao()->prepare($sql);
                 $p_sql->bindValue(":data_teste", $teste_vo2->getData_teste());
                 $p_sql->bindValue(":velocidade_inicial", $teste_vo2->getVelocidade_inicial());
-                $p_sql->bindValue(":fc_repouso", $dados_iniciais->getFc_repouso());
-                $p_sql->bindValue(":pressao_inicio", $dados_iniciais->getPressao_inicio());
-                $p_sql->bindValue(":pressao_final", $dados_iniciais->getPressao_final());
+                $p_sql->bindValue(":fc_repouso", $teste_vo2->getFc_repouso());
+                $p_sql->bindValue(":pressao_inicio", $teste_vo2->getPressao_inicio());
+                $p_sql->bindValue(":pressao_final", $teste_vo2->getPressao_final());
                 $p_sql->bindValue(":fc_teste", $teste_vo2->getFc_teste());
                 $p_sql->bindValue(":tempo_teste", $teste_vo2->getTempo_teste());
-                $p_sql->bindValue(":velocidade_teste", $dados_iniciais->getVelocidade_teste());
-                $p_sql->bindValue(":esforco_teste", $dados_iniciais->getEsforco_teste());
-                $p_sql->bindValue(":resultado_vo2", $dados_iniciais->getResultado_vo2());
+                $p_sql->bindValue(":velocidade_teste", $teste_vo2->getVelocidade_teste());
+                $p_sql->bindValue(":esforco_teste", $teste_vo2->getEsforco_teste());
+                $p_sql->bindValue(":resultado_vo2", $teste_vo2->getResultado_vo2());
 
                 return $p_sql->execute();
                 
@@ -63,7 +63,7 @@
             try {
                 $sql = "DELETE FROM Teste_VO2 WHERE idTeste_VO2 = :idTesteVO2";
                 $p_sql = Conexao::getConexao()->prepare($sql);
-                $p_sql->bindValue(":idTesteVO2", $dados_iniciais->getIdTesteVO2());
+                $p_sql->bindValue(":idTesteVO2", $teste_vo2->getIdTesteVO2());
                 return $p_sql->execute();
             } catch (Exception $e) {
                 echo "Erro ao Excluir dados iniciais <br> $e <br>";
@@ -81,8 +81,8 @@
                     fc_repouso=:fc_repouso, 
                     pressao_inicio=:pressao_inicio, 
                     pressao_final=:pressao_final, 
-                    fc_teste=:fc_teste, :
-                    tempo_teste=tempo_teste, 
+                    fc_teste=:fc_teste, 
+                    tempo_teste=:tempo_teste, 
                     velocidade_teste=:velocidade_teste, 
                     esforco_teste=:esforco_teste, 
                     resultado_vo2=:resultado_vo2
@@ -91,14 +91,14 @@
                 $p_sql = Conexao::getConexao()->prepare($sql);
                 $p_sql->bindValue(":data_teste", $teste_vo2->getData_teste());
                 $p_sql->bindValue(":velocidade_inicial", $teste_vo2->getVelocidade_inicial());
-                $p_sql->bindValue(":fc_repouso", $dados_iniciais->getFc_repouso());
-                $p_sql->bindValue(":pressao_inicio", $dados_iniciais->getPressao_inicio());
-                $p_sql->bindValue(":pressao_final", $dados_iniciais->getPressao_final());
+                $p_sql->bindValue(":fc_repouso", $teste_vo2->getFc_repouso());
+                $p_sql->bindValue(":pressao_inicio", $teste_vo2->getPressao_inicio());
+                $p_sql->bindValue(":pressao_final", $teste_vo2->getPressao_final());
                 $p_sql->bindValue(":fc_teste", $teste_vo2->getFc_teste());
                 $p_sql->bindValue(":tempo_teste", $teste_vo2->getTempo_teste());
-                $p_sql->bindValue(":velocidade_teste", $dados_iniciais->getVelocidade_teste());
-                $p_sql->bindValue(":esforco_teste", $dados_iniciais->getEsforco_teste());
-                $p_sql->bindValue(":resultado_vo2", $dados_iniciais->getResultado_vo2());
+                $p_sql->bindValue(":velocidade_teste", $teste_vo2->getVelocidade_teste());
+                $p_sql->bindValue(":esforco_teste", $teste_vo2->getEsforco_teste());
+                $p_sql->bindValue(":resultado_vo2", $teste_vo2->getResultado_vo2());
             
                 return $p_sql->execute();
             } catch (Exception $e) {
