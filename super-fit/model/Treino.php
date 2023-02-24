c7b339629beddc7785f7fe8aeb975ff1691ec880
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-02-24 14:02:34 -0300

    :sparkle: feat: criando model

---
+++ super-fit/model/Treino.php
@@ -0,0 +1,65 @@
+<?php
+
+    Class Treino{
+        private $idTreino;
+        private Serie $serie;
+        private Exercicios $exercicios;
+        private $carga;
+        private $feedback;
+        private Usuario $usuario;
+
+        function getIdTreino()
+        { 
+            return $this->idTreino; 
+        }
+        function setIdTreino($idTreino)
+        { 
+            $this->idTreino = $idTreino; 
+        }
+
+        function getSerie()
+        { 
+            return $this->serie; 
+        }
+        function setSerie($serie)
+        { 
+            $this->serie = $serie; 
+        }
+        
+        function getExercicios()
+        { 
+            return $this->exericios; 
+        }
+        function setExercicios($exercicios)
+        { 
+            $this->exercicios = $exercicios; 
+        }
+
+        function getCarga()
+        { 
+            return $this->carga; 
+        }
+        function setCarga($carga)
+        { 
+            $this->carga = $carga; 
+        }
+
+        function getFeedback()
+        { 
+            return $this->feedback; 
+        }
+        function setFeedback($feedback)
+        { 
+            $this->feedback = $feedback; 
+        }
+
+        function getUsuario()
+        { 
+            return $this->usuario; 
+        }
+        function setUsuario($usuario)
+        { 
+            $this->usuario = $usuario; 
+        }
+    }
+?>
