c7999f36313f497be1999eeaecae27ceb59d774d
Mario de Martini <97710585+mariodemartini@users.noreply.github.com>
2023-03-10 15:45:02 -0300

    :recycle: refactor: atualizando layout de cadastro

--- super-fit/cadastro-aluno.php
+++ super-fit/cadastro-aluno.php
@@ -4,93 +4,106 @@ include_once('include/header.html');
 include_once('include/navbar.html');
 include_once('include/sidebar.html');
 include_once('./conexao/Conexao.php');
-include_once('./model/Professor.php');
-include_once('./dao/ProfessorDAO.php');
+include_once('./model/Aluno.php');
+include_once('./dao/AlunoDAO.php');
 
-$professor = new Professor();
-$professordao = new ProfessorDAO();
+$aluno = new Aluno();
+$alunodao = new AlunoDAO();
 ?>
-<body>
 <main>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-lg-12">
+            <div class="col-lg-10">
                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                     <!-- Div do Titulo da página -->
-                    <div class="card-header"><h3 class="text-center font-weight-light my-4">CADASTRAR PROFESSOR</h3></div>
-                    <!-- Div do formulario principal --> 
+                    <div class="card-header"><h3 class="text-center font-weight-light my-4">CADASTRAR NOVO ALUNO</h3></div>
+                    <!-- Div do formulario principal -->
                     <div class="card-body">
-                        <form action="controller/ProfessorController.php" method="POST">
+                        <form action="controller/AlunoController.php" method="POST">
+                            <!-- Campo data -->
+                            <div class="row mb-3">
+                                <div class="form-group col-md-3">
+                                    <label for="inputData">DATA</label>
+                                    <input type="date" class="form-control" id="inputData" name="dataCadastro">
+                                </div>
+                                <div class="form-group col-md-3">
+
+                                </div>
+                            </div>
                             <!-- Campo Nome -->
                             <div class="row mb-3">
-                                <div class="col-md-5">
+                                <div class="col-md-12">
                                     <div class="form-floating mb-3 mb-md-0">
                                         <input class="form-control" id="inputNome" type="text" placeholder="nome" name="nome"/>
                                         <label for="inputNome">Nome Completo</label>
                                     </div>
                                 </div>
-                                <!-- Campo sexo -->
-                                <div class="col-md-3">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputSexo" type="text" placeholder="Sexo" name="sexo"/>
-                                        <label for="inputSexo">Sexo</label>
-                                    </div>
-                                </div>
+                            </div>
+                            <div class="row mb-3">
                                 <!-- Campo data nascimento -->
-                                <div class="col-md-3">
+                                <div class="col-md-6">
                                     <div class="form-floating mb-3 mb-md-0">
                                         <input class="form-control" id="inputDataNascimento" type="date" placeholder="Data de Nascimento" name="dataNascimento"/>
                                         <label for="inputDataNascimento">Data de Nascimento</label>
                                     </div>
                                 </div>
+                                <!-- Campo sexo -->
+                                <div class="col-md-6">
+                                    <div class="form-floating mb-3 mb-md-0">
+                                        <input class="form-control" id="inputSexo" type="text" placeholder="Sexo" name="sexo"/>
+                                        <label for="inputSexo">Sexo</label>
+                                    </div>
+                                </div>
                             </div>
                             <div class="row mb-3">
                                 <!-- Campo CPF -->
-                                <div class="col-md-4">
+                                <div class="col-md-6">
                                     <div class="form-floating mb-3 mb-md-0">
                                         <input class="form-control" id="inputCpf" type="text" placeholder="CPF" name="cpf"/>
                                         <label for="inputCpf">CPF</label>
                                     </div>
                                 </div>
-                                <!-- Campo CREF -->
-                                <div class="col-md-4">
+                                <!-- Campo RG -->
+                                <div class="col-md-6">
                                     <div class="form-floating">
-                                        <input class="form-control" id="inpuCREF" type="text" placeholder="CREF" name="cref"/>
-                                        <label for="inputCREF">CREF</label>
+                                        <input class="form-control" id="inpuRg" type="text" placeholder="RG" name="rg"/>
+                                        <label for="inputRg">RG</label>
                                     </div>
                                 </div>
                             </div>
                             <div class="row mb-3">
+                                <!-- Campo telefone -->
+                                <div class="col-md-6">
+                                    <div class="form-floating mb-3 mb-md-0">
+                                        <input class="form-control" id="inputTelefone" type="text" placeholder="(xx)xxxx-xxxx" name="telefone"/>
+                                        <label for="inputTelefone">Telefone</label>
+                                    </div>
+                                </div>
                                 <!-- Campo celular -->
-                                <div class="col-md-3">
+                                <div class="col-md-6">
                                     <div class="form-floating">
                                         <input class="form-control" id="inputCelular" type="text" placeholder="(xx)xxxx-xxxx" name="celular"/>
                                         <label for="inputCelular">Celular</label>
                                     </div>
                                 </div>
-                                <!-- Campo email -->
-                                <div class="col-md-5">
-                                    <div class="form-floating mb-3">
-                                        <input class="form-control" id="inputEmail" type="email" placeholder="nome@exemplo.com" name="email"/>
-                                        <label for="inputEmail">Email</label>
-                                    </div>
-                                </div>
                             </div>
                             <div class="row mb-3">
                                 <!-- Campo CEP -->
-                                <div class="col-md-4">
+                                <div class="col-md-6">
                                     <div class="form-floating mb-3 mb-md-0">
                                         <input class="form-control" id="inputCep" type="text" placeholder="CEP" name="cep"/>
                                         <label for="inputCep">CEP</label>
                                     </div>
                                 </div>
                                 <!-- Campo estado -->
-                                <div class="col-md-3">
+                                <div class="col-md-6">
                                     <div class="form-floating">
                                         <input class="form-control" id="inputEstado" type="text" placeholder="Estado" name="estado"/>
                                         <label for="inputEstado">Estado</label>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="row mb-3">
                                 <!-- Campo cidade -->
                                 <div class="col-md-4">
                                     <div class="form-floating mb-3 mb-md-0">
@@ -98,8 +111,6 @@ $professordao = new ProfessorDAO();
                                         <label for="inputCidade">Cidade</label>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="row mb-3">
                                 <!-- Campo endereço -->
                                 <div class="col-md-8">
                                     <div class="form-floating">
@@ -108,19 +119,17 @@ $professordao = new ProfessorDAO();
                                     </div>
                                 </div>
                             </div>
+                            <!-- Campo email -->
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="inputEmail" type="email" placeholder="nome@exemplo.com" name="email"/>
+                                <label for="inputEmail">Email</label>
+                            </div>
                             <div class="row mb-3">
                                 <!-- Campo senha -->
-                                <div class="col-md-3">
+                                <div class="col-md-6">
                                     <div class="form-floating mb-3 mb-md-0">
                                         <input class="form-control" id="inputPassword" type="password" placeholder="Senha" name="senha"/>
                                         <label for="inputPassword">Senha</label>
-                                    </div>
-                                </div>
-                                <!-- Campo  confirmar senha -->
-                                <div class="col-md-3">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputPasswordConfirm" type="password" placeholder="Confirmar senha" name="senha"/>
-                                        <label for="inputPasswordConfirm">Confirmar Senha</label>
                                     </div>
                                 </div>
                             </div>
@@ -129,7 +138,6 @@ $professordao = new ProfessorDAO();
                                 <button type="submit" name="salvar" class="btn btn-success btn-lg">Salvar</button>
                                 <button type="button" class="btn btn-secondary btn-sm"><a class="btn btn-secondary btn-block" href="cadastro.php">Voltar</a></button>
                             </div>
-                            <br>
                         </form>
                     </div>
                     <div class="card-footer text-center py-3"></div>
@@ -138,7 +146,6 @@ $professordao = new ProfessorDAO();
         </div>
     </div>
 </main>
-</body>
 <!-- Include php rodapé -->
 <?php
 include_once('include/footer.html');
--- super-fit/cadastro.php
+++ super-fit/cadastro.php
@@ -3,152 +3,39 @@
 include_once('include/header.html');
 include_once('include/navbar.html');
 include_once('include/sidebar.html');
-include_once('./conexao/Conexao.php');
-include_once('./model/Aluno.php');
-include_once('./dao/AlunoDAO.php');
-
-$aluno = new Aluno();
-$alunodao = new AlunoDAO();
 ?>
 <main>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-lg-10">
+            <div class="col-lg-8 text-center">
+                <!-- Div do Titulo da página e barra de pesquisa -->
                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                    <!-- Div do Titulo da página -->
-                    <div class="card-header"><h3 class="text-center font-weight-light my-4">CADASTRAR NOVO ALUNO</h3></div>
-                    <!-- Div do formulario principal -->
+                    <div class="card-header"><h3 class="text-center font-weight-light my-4">CADASTRO</h3></div>
+                    <!-- Div do conteudo principal e botões -->
                     <div class="card-body">
-                        <form action="controller/AlunoController.php" method="POST">
-                            <!-- Campo data -->
-                            <div class="row mb-3">
-                                <div class="form-group col-md-3">
-                                    <label for="inputData">DATA</label>
-                                    <input type="date" class="form-control" id="inputData" name="dataCadastro">
-                                </div>
-                                <div class="form-group col-md-3">
+                        <!-- Sub-menu -->
+                        <div class="row mb-3">
+                            <a class="btn btn-success btn-block" href="cadastro-aluno.php"><button type="button" class="btn btn-success btn-lg btn-block">NOVO ALUNO</button></a>
+                        </div>
 
-                                </div>
-                            </div>
-                            <!-- Campo Nome -->
-                            <div class="row mb-3">
-                                <div class="col-md-12">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputNome" type="text" placeholder="nome" name="nome"/>
-                                        <label for="inputNome">Nome Completo</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <!-- Campo data nascimento -->
-                                <div class="col-md-6">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputDataNascimento" type="date" placeholder="Data de Nascimento" name="dataNascimento"/>
-                                        <label for="inputDataNascimento">Data de Nascimento</label>
-                                    </div>
-                                </div>
-                                <!-- Campo sexo -->
-                                <div class="col-md-6">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputSexo" type="text" placeholder="Sexo" name="sexo"/>
-                                        <label for="inputSexo">Sexo</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <!-- Campo CPF -->
-                                <div class="col-md-6">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputCpf" type="text" placeholder="CPF" name="cpf"/>
-                                        <label for="inputCpf">CPF</label>
-                                    </div>
-                                </div>
-                                <!-- Campo RG -->
-                                <div class="col-md-6">
-                                    <div class="form-floating">
-                                        <input class="form-control" id="inpuRg" type="text" placeholder="RG" name="rg"/>
-                                        <label for="inputRg">RG</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <!-- Campo telefone -->
-                                <div class="col-md-6">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputTelefone" type="text" placeholder="(xx)xxxx-xxxx" name="telefone"/>
-                                        <label for="inputTelefone">Telefone</label>
-                                    </div>
-                                </div>
-                                <!-- Campo celular -->
-                                <div class="col-md-6">
-                                    <div class="form-floating">
-                                        <input class="form-control" id="inputCelular" type="text" placeholder="(xx)xxxx-xxxx" name="celular"/>
-                                        <label for="inputCelular">Celular</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <!-- Campo CEP -->
-                                <div class="col-md-6">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputCep" type="text" placeholder="CEP" name="cep"/>
-                                        <label for="inputCep">CEP</label>
-                                    </div>
-                                </div>
-                                <!-- Campo estado -->
-                                <div class="col-md-6">
-                                    <div class="form-floating">
-                                        <input class="form-control" id="inputEstado" type="text" placeholder="Estado" name="estado"/>
-                                        <label for="inputEstado">Estado</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <!-- Campo cidade -->
-                                <div class="col-md-4">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputCidade" type="text" placeholder="Cidade" name="cidade"/>
-                                        <label for="inputCidade">Cidade</label>
-                                    </div>
-                                </div>
-                                <!-- Campo endereço -->
-                                <div class="col-md-8">
-                                    <div class="form-floating">
-                                        <input class="form-control" id="inputEndereco" type="text" placeholder="endereco" name="endereco"/>
-                                        <label for="inputEndereco">Rua, Nº, Bairro</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Campo email -->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="inputEmail" type="email" placeholder="nome@exemplo.com" name="email"/>
-                                <label for="inputEmail">Email</label>
-                            </div>
-                            <div class="row mb-3">
-                                <!-- Campo senha -->
-                                <div class="col-md-6">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputPassword" type="password" placeholder="Senha" name="senha"/>
-                                        <label for="inputPassword">Senha</label>
-                                    </div>
-                                </div>
-                                <!-- Campo  confirmar senha -->
-                                <div class="col-md-6">
-                                    <div class="form-floating mb-3 mb-md-0">
-                                        <input class="form-control" id="inputPasswordConfirm" type="password" placeholder="Confirmar senha" name="senha"/>
-                                        <label for="inputPasswordConfirm">Confirmar Senha</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Botões de salvar -->
-                            <div class="mt-4 mb-0">
-                                <button type="submit" name="salvar" class="btn btn-success btn-lg">Salvar</button>
-                                <button type="button" class="btn btn-primary btn-sm"><a class="btn btn-primary btn-block" href="buscar.php">Buscar</a></button>
-                                <button type="button" class="btn btn-secondary btn-sm"><a class="btn btn-secondary btn-block" href="home.php">Voltar</a></button>
-                            </div>
-                        </form>
+                        <div class="row mb-3">
+                            <a class="btn btn-info btn-block" href="buscar-aluno.php"><button type="button" class="btn btn-info btn-lg btn-block">PESQUISAR ALUNO</button></a>
+                        </div>
+
+                        <br><br>
+
+
+                        <div class="row mb-3">
+                        <a class="btn btn-success btn-block" href="cadastro-prof.php"><button type="button" class="btn btn-success btn-lg btn-block">NOVO PROFESSOR</button></a>
+                        </div>
+
+                        <div class="row mb-3">
+                        <a class="btn btn-info btn-block" href="buscar-prof.php"><button type="button" class="btn btn-info btn-lg btn-block">PESQUISAR PROFESSOR</button></a>
+                        </div> 
                     </div>
-                    <div class="card-footer text-center py-3"></div>
+                    <div class="mt-4 mb-0">
+                        <button type="button" class="btn btn-secondary btn-sm"><a class="btn btn-secondary btn-block" href="home.php">Voltar</a></button>
+                    </div>
                 </div>
             </div>
         </div>
